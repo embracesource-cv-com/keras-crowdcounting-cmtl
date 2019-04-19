df46d2e50756320e8d80938f1db85215d176a977
ybcc <271380178@qq.com>
2019-04-19 10:03:46 +0800

    adjust metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -8,9 +8,39 @@
 import keras.backend as K
 
 
-def MAE(y_true, y_pred):
-    return K.abs(K.sum(y_true) - K.sum(y_pred))
+def mae(y_true, y_pred):
+    """
+    mean absolute error
+    :param y_true: shape(batch_size, h, w, 1)
+    :param y_pred: shape(batch_size, h, w, 1)
+    :return: float
+    """
+    shape = K.shape(y_pred)
+    h, w = shape[1], shape[2]
+    y_true = K.reshape(y_true, [-1, h * w])
+    y_pred = K.reshape(y_pred, [-1, h * w])
+    loss = K.abs(K.sum(y_true, axis=-1) - K.sum(y_pred, axis=-1))
+    return K.mean(loss)
 
 
-def MSE(y_true, y_pred):
-    return (K.sum(y_true) - K.sum(y_pred)) * (K.sum(y_true) - K.sum(y_pred))
+def mse(y_true, y_pred):
+    """
+    mean square error
+    :param y_true: shape(batch_size, h, w, 1)
+    :param y_pred: shape(batch_size, h, w, 1)
+    :return: float
+    """
+    shape = K.shape(y_pred)
+    h, w = shape[1], shape[2]
+    y_true = K.reshape(y_true, [-1, h * w])
+    y_pred = K.reshape(y_pred, [-1, h * w])
+    loss = K.square(K.sum(y_true, axis=-1) - K.sum(y_pred, axis=-1))
+    return K.sqrt(K.mean(loss))
+
+
+# def MAE(y_true, y_pred):
+#     return K.abs(K.sum(y_true) - K.sum(y_pred))
+#
+#
+# def MSE(y_true, y_pred):
+#     return (K.sum(y_true) - K.sum(y_pred)) * (K.sum(y_true) - K.sum(y_pred))
