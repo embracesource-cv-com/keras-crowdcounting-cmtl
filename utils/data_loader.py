425d48f27c1397debdd00528838bf3abbfa43118
yizt <csuyzt@163.com>
2019-05-30 15:52:58 +0800

    重构data loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -10,14 +10,14 @@ import pandas as pd
 import cv2
 import os
 import sys
+from keras.utils import Sequence
 
 
-class DataLoader(object):
-    def __init__(self, data_path, gt_path, shuffle=False, gt_downsample=False, num_classes=10):
+class DataLoader(Sequence):
+    def __init__(self, data_path, gt_path, shuffle=False, num_classes=10):
         self.data_path = data_path
         self.gt_path = gt_path
         self.shuffle = shuffle
-        self.gt_downsample = gt_downsample
         self.num_classes = num_classes
         self.data_files = [filename for filename in os.listdir(data_path)]
         self.num_samples = len(self.data_files)
@@ -26,6 +26,21 @@ class DataLoader(object):
         self.min_gt_count = sys.maxsize
         self.bin = 0
         self.count_class_hist = np.zeros(num_classes)
+        self.load_all()
+
+    def __getitem__(self, item):
+        x = self.blob_list[item]['data']
+        den = self.blob_list[item]['gt_den']
+        cls = self.blob_list[item]['gt_class']
+        # 增加batch 维返回
+        return x[np.newaxis, ...], {"density": den[np.newaxis, ...], "cls": cls[np.newaxis, ...]}
+
+    def __len__(self):
+        return len(self.blob_list)
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            np.random.shuffle(self.blob_list)
 
     def load_all(self):
         """
@@ -44,16 +59,13 @@ class DataLoader(object):
             ht_1 = ht // 4 * 4
             wd_1 = wd // 4 * 4
             img = cv2.resize(img, (wd_1, ht_1))
-            img = img.reshape((img.shape[0], img.shape[1], 1))
+            # 加载密度图
             den = pd.read_csv(os.path.join(self.gt_path, os.path.splitext(fname)[0] + '.csv'),
                               header=None).values
             den = den.astype(np.float32, copy=False)
-            if self.gt_downsample:
-                wd_1 = wd_1 // 4
-                ht_1 = ht_1 // 4
             den = cv2.resize(den, (wd_1, ht_1))
             den = den * ((wd * ht) / (wd_1 * ht_1))
-            den = den.reshape((den.shape[0], den.shape[1], 1))
+            # 人头数
             gt_count = np.sum(den)
             self.min_gt_count = min(self.min_gt_count, gt_count)
             self.max_gt_count = max(self.max_gt_count, gt_count)
@@ -66,15 +78,15 @@ class DataLoader(object):
 
             if i % 100 == 0:
                 print('Loaded {}/{} files'.format(i, self.num_samples))
-        print('[INFO] Completed loading {} files.'.format(i))
+        print('[INFO] Completed loading {} files.'.format(len(self.data_files)))
 
         self.assign_classes()  # 设置图片类别
         if self.shuffle:
             np.random.shuffle(self.blob_list)
-        X = np.array([blob['data'] for blob in self.blob_list])
-        Y_den = np.array([blob['gt_den'] for blob in self.blob_list])
-        Y_class = np.array([blob['gt_class'] for blob in self.blob_list])
-        return X, Y_den, Y_class
+        xs = np.array([blob['data'] for blob in self.blob_list])
+        ys_den = np.array([blob['gt_den'] for blob in self.blob_list])
+        ys_class = np.array([blob['gt_class'] for blob in self.blob_list])
+        return xs, ys_den, ys_class
 
     def assign_classes(self):
         """
@@ -84,7 +96,7 @@ class DataLoader(object):
         for blob in self.blob_list:
             gt_class = np.zeros(self.num_classes, dtype=np.int32)
             idx = np.round(blob['gt_count'] / self.bin)
-            idx = int(min(idx, self.num_classes-1))
+            idx = int(min(idx, self.num_classes - 1))
             gt_class[idx] = 1
             blob['gt_class'] = gt_class
             self.count_class_hist[idx] += 1
@@ -94,21 +106,8 @@ class DataLoader(object):
         根据每类的样本数量对每一类设置权重（可在训练期间让模型更多关注样本较少的类别）
         """
         class_weights = 1 - self.count_class_hist / self.num_samples
-        class_weights = class_weights / self.num_samples
+        class_weights = class_weights / np.sum(class_weights)
         return class_weights
-
-    def flow(self, batch_size=32):
-        # todo: generate data in batch to save memory
-        loop_count = self.num_samples // batch_size
-        while True:
-            np.random.shuffle(self.blob_list)
-            for i in range(loop_count):
-                blobs = self.blob_list[i*batch_size: (i+1)*batch_size]
-                X = np.array([blob['data'] for blob in blobs])
-                Y_den = np.array([blob['gt_den'] for blob in blobs])
-                X, Y_den = self.random_augment(X, Y_den)  # 随机增广
-                Y_class = np.array([blob['gt_class'] for blob in blobs])
-                yield X, Y_den, Y_class
 
     @staticmethod
     def random_augment(imgs, gts):
@@ -126,7 +125,3 @@ class DataLoader(object):
             imgs_aug.append(img)
             gts_aug.append(gt)
         return np.array(imgs_aug), np.array(gts_aug)
-
-    def __iter__(self):
-        for blob in self.blob_list:
-            yield blob
