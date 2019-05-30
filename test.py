1c017a79cc10115d42edfeb6ba99cdd826baf4c4
yizt <csuyzt@163.com>
2019-05-30 16:07:49 +0800

    重构预测过程

--- test.py
+++ test.py
@@ -5,40 +5,36 @@
    Author:        steven.yi
    date:          2019/04/17
 """
-from keras.models import load_model
+from model import CMTL
 from utils.data_loader import DataLoader
-from utils.heatmap import save_heatmap
-from utils.spp import SpatialPyramidPooling
 import numpy as np
 from config import current_config as cfg
 import argparse
 import os
-import tensorflow as tf
 
 
 def main(args):
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     dataset = args.dataset  # 'A' or 'B'
-    if dataset == 'A':
-        model_path = './trained_models/mcnn_A_train.hdf5'
-    else:
-        model_path = './trained_models/mcnn_B_train.hdf5'
+    output_dir = args.output_dir
+    weight_path = args.weight_path
+    cfg.init_path(dataset)
 
-    output_dir = './output_{}/'.format(dataset)
     heatmaps_dir = os.path.join(output_dir, 'heatmaps')  # directory to save heatmap
     results_txt = os.path.join(output_dir, 'results.txt')  # file to save predicted results
     for _dir in [output_dir, heatmaps_dir]:
         if not os.path.exists(_dir):
             os.mkdir(_dir)
 
-    test_path = cfg.TEST_PATH.format(dataset)
-    test_gt_path = cfg.TEST_GT_PATH.format(dataset)
     # load test set
-    data_loader = DataLoader(test_path, test_gt_path, shuffle=False, gt_downsample=True)
-    _ = data_loader.load_all()
+    data_loader = DataLoader(cfg.TEST_PATH,
+                             cfg.TEST_GT_PATH,
+                             shuffle=False,
+                             gt_downsample=True)
     # load model
     print('[INFO] Load model ...')
-    model = load_model(model_path, custom_objects={'tf': tf})
+    model = CMTL(input_shape=(None, None, 1))
+    model.load_weights(weight_path, by_name=True)
 
     # test
     print('[INFO] Testing Part_{} ...'.format(dataset))
@@ -62,7 +58,7 @@ def main(args):
         # save results
         with open(results_txt, 'a') as f:
             line = '<{}> {:.2f}--{:.2f}\t{}--{}\n'.format(blob['fname'].split('.')[0], gt_count, pred_count,
-                                                              gt_cls, np.argmax(pred_cls[0]))
+                                                          gt_cls, np.argmax(pred_cls[0]))
             f.write(line)
 
     mae = mae / data_loader.num_samples
@@ -75,6 +71,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("dataset", help="the dataset you want to predict", choices=['A', 'B'])
+    parser.add_argument("--dataset", default="A", help="the dataset you want to predict", choices=['A', 'B'])
+    parser.add_argument("--weight_path", type=str, default="tmp/cmtl-A.h5", help="weight path")
+    parser.add_argument("--output_dir", default="./", help="output directory to save predict heatmaps")
+
     args = parser.parse_args()
     main(args)
